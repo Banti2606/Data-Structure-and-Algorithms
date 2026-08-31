511b2053faf156329d1f4af6f15f61fd785f6a51
Banti2606 <banti152666@gmail.com>
2026-08-31 16:09:18 +0530

    Time: 43 ms (59.83%) | Memory: 84.9 MB (34.54%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+
+        unordered_map<int, int> mp;
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < fruits.size(); right++) {
+
+            // Add current fruit
+            mp[fruits[right]]++;
+
+            // More than 2 types
+            while (mp.size() > 2) {
+
+                mp[fruits[left]]--;
+
+                if (mp[fruits[left]] == 0)
+                    mp.erase(fruits[left]);
+
+                left++;
+            }
+
+            // Current valid window
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
